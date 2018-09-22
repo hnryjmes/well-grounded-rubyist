40d560d81953043c8b25fe2e4c011a818991b1b2
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-22 13:06:21 +0100

    regex

--- regex.rb
+++ regex.rb
@@ -1,2 +1,5 @@
 puts "Match!" if /abc/.match("The alphabet starts with abc.")
 puts "Match!" if "The alphabet starts with abc.".match(/abc/)
+
+puts "Match!" if /abc/ =~ "The alphabet starts with abc."
+puts "Match!" if "The alphabet starts with abc." =~ /abc/
